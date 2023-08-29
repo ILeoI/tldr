c6dd2ad0b3784bfe16951268eac6012cf8ca8008
ILeoI <leoj.whiteside@gmail.com>
2023-08-29 15:19:15 +0930

    Changed signup

--- signup-page.php
+++ signup-page.php
@@ -10,15 +10,30 @@
 
 <body>
     <h1 class="page-title" id="signup-page-header">Signup Page</h1>
-    <form id="signup-form" action="login-page.php" method="POST">
-        <label for="username">Username</label> <br>
-        <input type="text" placeholder="Username" id="username" name="username"> <br>
+    <form id="signup-form" action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST">
+        <label for="email">E-mail</label> <br>
+        <input type="email" placeholder="E-mail" id="email" name="email"> <br>
 
         <label for="password">Password</label> <br>
-        <input type="text" placeholder="Password" id="password" name="password"> <br>
+        <input type="text" placeholder="Password" id="password" name="password" minlength="8"> <br>
+
+        <label for="confirm-password">Confirm Password</label> <br>
+        <input type="text" placeholder="Confirm Password" id="confirm-password" name="confirm-password" minlength="8">
 
         <input type="submit" value="Sign up">
     </form>
 </body>
 
 </html>
+
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = filter_input(INPUT_POST, "email", FILTER_SANITIZE_SPECIAL_CHARS);
+    $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+    $confirm_password = filter_input(INPUT_POST, "confirm-password", FILTER_SANITIZE_SPECIAL_CHARS);
+
+    
+}
+
+?>
