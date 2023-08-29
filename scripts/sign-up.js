9c61cef6c5698130524a8198dd1e0a1c1494985b
ILeoI <leoj.whiteside@gmail.com>
2023-08-29 17:10:01 +0930

    Added password matching

--- scripts/sign-up.js
+++ scripts/sign-up.js
@@ -1,3 +1,4 @@
+// Show hide passwords
 function togglePassword() {
     const password = document.getElementById("password");
     const confirmPassword = document.getElementById("confirm-password");
@@ -10,3 +11,26 @@ function togglePassword() {
         confirmPassword.type = "text";
     }
 }
+
+// Check if password and confirm password don't match
+function validatePasswords() {
+    const password = document.getElementById("password");
+    const confirmPassword = document.getElementById("confirm-password");
+
+    if (password.value != confirmPassword.value) {
+        confirmPassword.setCustomValidity("Passwords do not match");
+    } else {
+        confirmPassword.setCustomValidity("");
+    }
+
+}
+
+// Init Function, stuff to be run on launch
+function init() {
+    const password = document.getElementById("password");
+    const confirmPassword = document.getElementById("confirm-password");
+    password.onchange = validatePasswords;
+    confirmPassword.onkeyup = validatePasswords;
+}
+
+init();
