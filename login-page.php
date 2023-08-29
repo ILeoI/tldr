210d356288164f46bbb0b50dbc7e5defc1809679
Nesquick5 <nesb0028@flinders.edu.au>
2023-08-29 15:20:19 +0930

    styling

--- login-page.php
+++ login-page.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="author" content="">
@@ -7,16 +8,20 @@
     <title>Login</title>
     <link rel="stylesheet" href="Style/login.css" />
 </head>
+
 <body>
-    <h1 class="page-title" id="login-page-header">Login Page</h1>
-    <form id="login-form" action="home-page.php" method="POST">
-        <label for="username">Username</label> <br>
-        <input type="text" placeholder="Username" id="username" name="username"> <br>
+    <div class="login">
+        <h1 class="page-title" id="login-page-header">Login Page</h1>
+        <form id="login-form" action="home-page.php" method="POST">
+            <label for="username">Username</label> <br>
+            <input type="text" placeholder="Username" id="username" name="username"> <br>
 
-        <label for="password">Password</label> <br>
-        <input type="text" placeholder="Password" id="password" name="password"> <br>
+            <label for="password">Password</label> <br>
+            <input type="text" placeholder="Password" id="password" name="password"> <br>
 
-        <input type="submit" value="Login">
-    </form>
+            <input type="submit" value="Login">
+        </form>
+    </div>
 </body>
+
 </html>
