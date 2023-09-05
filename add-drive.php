33fec56f889e74e0e10ecb6e7087d33f5442adf2
coby-roberts <104502547+coby-roberts@users.noreply.github.com>
2023-09-05 15:48:38 +0930

    php for add-drive page

--- add-drive.php
+++ add-drive.php
@@ -3,6 +3,26 @@
 if (isset($_POST["add_drive"])) {
    require_once "inc/dbconn.inc.php"; 
    
+    if ($_POST["time"] == "daytime"){
+        $daytime = 1;
+    } else {
+        $daytime = 0;
+    }
+
+    $learnerID = 0;
+    $driveDate = $_POST["dob"];
+    $startTime = $_POST["start-time"];
+    $endTime = $_POST["end-time"];
+    $duration = 0; //create function to calculate drive time off start and finsih
+    $fromLoc = filter_input(INPUT_POST, "start-location", FILTER_SANITIZE_SPECIAL_CHARS);
+    $toLoc = filter_input(INPUT_POST, "furthest-location", FILTER_SANITIZE_SPECIAL_CHARS);
+    $conditionRoad = $_POST["road-type"];
+    $conditionWeather = $_POST["weather"];
+    $conditionTraffic = $_POST["traffic-density"];
+    $daytime = 
+    $supervisingDriverID =
+    $verified =
+    
     $sql = "INSERT INTO Drive() VALUES(?);";
 
     $statement = mysqli_stmt_init($conn);
