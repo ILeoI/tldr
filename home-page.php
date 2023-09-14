8ef87e9cadc41753f2dc986c2d8319df26a7342f
jacobmrCuzzins <jacob.cousinsflinders@gmail>
2023-09-14 15:06:47 +0930

    created base home page layout

--- home-page.php
+++ home-page.php
@@ -2,12 +2,32 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="author" content="">
-    <meta name="description" content="">
+    <meta name="author" content="Jacob">
+    <meta name="description" content="HomePage">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>TLDR: Home Page</title>
+    <link rel="stylesheet" href="style/home-page.css" />
+    <script src="scripts/home.js" defer></script>
 </head>
+<h1 class="page-title" id="home-page-header">
 <body>
-    <h1 class="page-title" id="home-page-header">Home Page</h1>
-    <p>Logged In</p>
+
+<div class="dropdown">
+  <button class="dropbtn">
+  <div class="bar"></div>
+  <div class="bar"></div>
+  <div class="bar"></div>
+  
+  </button>
+  <div class="dropdown-content">
+    <a href="drives.php">Drives Log</a>
+    <a href="cbta.php">CBTA</a>
+    <a href="your-account.php">Account</a>
+  </div>
+</div>
+    
+
+  Home Page</h1>
+    <p>Welcome, user</p>
 </body>
 </html>
