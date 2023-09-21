d8114121213a3ff026ea815ad0c90c02de1514d4
coby-roberts <104502547+coby-roberts@users.noreply.github.com>
2023-09-21 22:14:14 +0930

    update

--- drives.php
+++ drives.php
@@ -34,61 +34,97 @@ require_once "inc/dbconn.inc.php";
 
         if ($result = mysqli_query($conn, $sql)) {
             if (mysqli_num_rows($result) > 0) {
-                echo "<ul id=\"verify-drive\">";
-                while ($row = mysqli_fetch_assoc($result)) {
-                    print_r($row);
-                }
-                echo "</ul>";
-
-                mysqli_free_result($result);
+                $row = mysqli_fetch_assoc($result);
             }
         }
-
         ?>
+        <div class="verify-container">
+            <?php
+            // Retrieve unverified drives
+            $sql = "SELECT *
+                FROM Drives
+                WHERE verified =0;";
 
-    </div>
+            echo "<table>
+                <tr>
+                    <caption>Verify Drives</caption>
+                    <th>Supervising Driver</th>
+                    <th>Date/th>
+                    <th>Start Time</th> 
+                    <th>End Time</th>
+                    <th>Duration</th>
+                    <th>Start Location</th>
+                    <th>End Location</th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
 
-    <h1>Drive History</h1>
+                </tr>";
 
-    <div class="center" id="drive-history">
-
-        <?php
-
-        $sql = "SELECT * FROM Drives WHERE verified=1 AND learnerLicenseNo = '$licenseNo';";
-
-        if ($result = mysqli_query($conn, $sql)) {
-            if (mysqli_num_rows($result) > 0) {
-
-                echo "<ul id=\"history\">";
-
-                while ($row = mysqli_fetch_assoc($result)) {
-                    print_r($row);
-
+            if ($result = mysqli_query($conn, $sql)) {
+                if (mysqli_num_rows($result) > 0) {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        echo "<tr>
+                            <td>{$row['firstName']} {$row['lastName']}</td>
+                            <td>{$row['time']}</td>
+                            <td>{$row['location']}</td>
+                          </tr>";
+                    }
+                } else {
+                    echo "<tr><td colspan='3'>No bookings found.</td></tr>";
                 }
-                echo "</ul>";
-
-                mysqli_free_result($result);
             }
-        }
-        mysqli_close($conn);
-        ?>
+
+            echo "</table>";
 
 
-    </div>
+            ?>
 
-    
+        </div>
+
+        <h1>Drive History</h1>
+
+        <div class="center" id="drive-history">
+
+            <?php
+
+            $sql = "SELECT * FROM Drives WHERE verified=1 AND learnerLicenseNo = '$licenseNo';";
+
+            if ($result = mysqli_query($conn, $sql)) {
+                if (mysqli_num_rows($result) > 0) {
+
+                    echo "<ul id=\"history\">";
+
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        print_r($row);
+                    }
+                    echo "</ul>";
+
+                    mysqli_free_result($result);
+                }
+            }
+            mysqli_close($conn);
+            ?>
+
+
+        </div>
+
+
 </body>
 
 </html>
 
-<?php 
+<?php
 
 
 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
-    
+
+
 
     print_r($_POST);
 
@@ -98,13 +134,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $endTime = $_POST["end-time"];
     $fromLoc = filter_input(INPUT_POST, "start-location", FILTER_SANITIZE_SPECIAL_CHARS);
     $toLoc = filter_input(INPUT_POST, "furthest-location", FILTER_SANITIZE_SPECIAL_CHARS);
-    $road = $_POST["road-type"] . ' ' . $_POST["road-traffic"]; 
+    $road = $_POST["road-type"] . ' ' . $_POST["road-traffic"];
     $conditionWeather = $_POST["weather"];
     $conditionTraffic = $_POST["traffic-density"];
     $daytime = $_POST["time"];
     $supervisingDriverID = 555;
     $verified = 1;
-    
+
     $sql = "INSERT INTO Drives(userID, driveDate, startTime, endTime, fromLoc, toLoc, conditionRoad, conditionWeather, conditionTraffic, daytime, supervisingDriverID, verified) 
             VALUES('$learnerID', '$driveDate', '$startTime', '$endTime', '$fromLoc', '$toLoc', '$road', '$conditionWeather', '$conditionTraffic', '$daytime', '$supervisingDriverID', '$verified');";
 
