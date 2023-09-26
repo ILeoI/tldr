4de2f59e09b39dce30079d27d3c0503b73cb2599
ILeoI <leoj.whiteside@gmail.com>
2023-09-26 15:09:04 +0930

    Fixed up some shebang

--- instructors/instructor-menu.php
+++ instructors/instructor-menu.php
@@ -1,17 +1,16 @@
 <h1 class="page-title" id="home-page-header">
-        <div class="dropdown">
-            <button class="dropbtn">
-                <div class="bar"></div>
-                <div class="bar"></div>
-                <div class="bar"></div>
-            </button>
-            <div class="dropdown-content">
-                <a href="add-drive.php">Add Drive</a>
-                <a href="instructors/cbta.php">CBTA</a>
-                <a href="instructors/students.php">Your Students</a>
-                <a href="instructors/my-account.php">Account</a>
-                <a href="home-page.php">Home</a>
-            </div>
+    <div class="dropdown">
+        <button class="dropbtn">
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+        </button>
+        <div class="dropdown-content">
+            <a href="home-page.php">Home</a>
+            <a href="add-drive.php">Add Drive</a>
+            <a href="instructors/students.php">Your Students</a>
+            <a href="instructors/my-account.php">Account</a>
         </div>
-        <label>Home Page</label>
-    </h1>
+    </div>
+    <label>Home Page</label>
+</h1>
